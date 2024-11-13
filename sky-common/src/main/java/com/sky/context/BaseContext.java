b95a4e35004aa86f18497a41b0622b6055f9dbaa
13626646811 <1216951702@qq.com>
2024-11-13 14:56:45 +0800

    更新

--- sky-common/src/main/java/com/sky/context/BaseContext.java
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -3,7 +3,7 @@ package com.sky.context;
 public class BaseContext {
 
     public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
-
+    //在局部线程存入id
     public static void setCurrentId(Long id) {
         threadLocal.set(id);
     }
